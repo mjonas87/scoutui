325111b5edbf4bcd561b18411bf3ac57a99d921a
h20dragon <h20dragon@outlook.com>
2015-10-05 14:13:49 -0400

    Update version to 0.1.3

--- lib/scoutui/eyes/eye_factory.rb
+++ lib/scoutui/eyes/eye_factory.rb
@@ -26,23 +26,32 @@ module Scoutui::Eyes
       puts __FILE__ + (__LINE__).to_s + " create(#{use_eyes})" if Scoutui::Utils::TestUtils.instance.isDebug?
       eyes=nil
 
+
       if use_eyes
+        license_key=nil
 
         licFile=Scoutui::Utils::TestUtils.instance.getLicenseFile()
 
-        valid_json=false
-        begin
-          jFile = File.read(licFile)
-          jLicense=jsonData=JSON.parse(jFile)
-          valid_json=true
-        rescue => ex
-          ;
+        if !licFile.empty?
+
+          valid_json=false
+          begin
+            jFile = File.read(licFile)
+            jLicense=jsonData=JSON.parse(jFile)
+            license_key=jLicense['api_key'].to_s
+            valid_json=true
+          rescue => ex
+            ;
+          end
+
+        elsif ENV.has_key?('APPLITOOLS_API_KEY')
+          license_key=ENV['APPLITOOLS_API_KEY'].to_s
         end
 
 
-        if valid_json
+        if !license_key.nil?
           eyes=Applitools::Eyes.new()
-          eyes.api_key = jLicense['api_key'].to_s
+          eyes.api_key = license_key
           eyes.force_fullpage_screenshot = true
 
           match_level = Scoutui::Base::UserVars.instance.getVar('eyes.match_level')
