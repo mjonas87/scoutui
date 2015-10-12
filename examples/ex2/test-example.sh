f80345383748eb761d13a963af0a288f32253959
h20dragon <h20dragon@outlook.com>
2015-10-12 14:10:00 -0700

    Example 2.

---
+++ examples/ex2/test-example.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+##
+# Description
+#   This test script overrides the provided title and appname (if provided in the test config. file)
+#
+# Set the APPLITOOLS_API_KEY environment variable with your key.
+#
+#          export APPLITOOLS_API_KEY="__YOUR_KEY_HERE__"
+##
+SCOUTUI_BIN=../../bin/scoutui_driver.rb
+# Specify browser under test  (chrome, firefox, ie, safari)
+BUT=chrome
+
+# Specify the title and appName needed by Applitools
+TITLE=DEMO-ONE
+APP=Elvis
+
+# Specify the test configuration file
+TEST_CFG="./test.config.json"
+
+##
+# content
+# strict
+# exact
+# layyout
+##
+MATCH_TYPE="layout"
+
+#EYES=--eyes
+EYES=""
+
+# The following command line parameters will override provided title and appName (if provided in test config file)
+$SCOUTUI_BIN  --debug --config $TEST_CFG  --browser $BUT $EYES --app $APP --title $TITLE  --match $MATCH_TYPE --pagemodel ./page_model.json
+
+# The following
+# $SCOUTUI_BIN --config $TEST_CFG  --eyes  --match $MATCH_TYPE  --browser $BUT
