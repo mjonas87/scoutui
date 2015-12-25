135f67ae123bec58544a452da8102544237b0dfb
h20dragon <h20dragon@outlook.com>
2015-12-25 00:29:44 -0500

    Update version to 0.1.4.pre

--- lib/scoutui/base/q_browser.rb
+++ lib/scoutui/base/q_browser.rb
@@ -1,7 +1,7 @@
 
 require 'eyes_selenium'
 require 'selenium-webdriver'
-
+#require 'testmgr'
 
 module Scoutui::Base
 
@@ -36,24 +36,59 @@ module Scoutui::Base
       rc
     end
 
-    def self.getObject(drv, xpath, _timeout=30)
+    def self.getObject(drv, obj, _timeout=30)
+      puts __FILE__ + (__LINE__).to_s + " getObject(#{obj})  class:#{obj.class.to_s}   hash : #{obj.is_a?(Hash)}"
       rc=nil
+      visible_when=nil
+      locator=obj
+
       begin
 
-        if !xpath.match(/^page\([\w\d]+\)/).nil?
+        if obj.is_a?(Hash)
 
-          xpath = Scoutui::Utils::TestUtils.instance.getPageElement(xpath)
-          puts __FILE__ + (__LINE__).to_s + " Process page request #{xpath} => #{xpath}" if Scoutui::Utils::TestUtils.instance.isDebug?
+
+          if obj.has_key?('visible_when')
+            visible_when=!obj['visible_when'].match(/always/i).nil?
+          end
+
+          locator = obj['locator'].to_s
+
+
+        elsif !obj.match(/^page\([\w\d]+\)/).nil?
+
+          elt = Scoutui::Utils::TestUtils.instance.getPageElement(obj)
+
+          if elt.is_a?(Hash)
+            puts __FILE__ + (__LINE__).to_s + " JSON or Hash => #{elt}"
+            locator = elt['locator'].to_s
+          else
+            locator=elt.to_s
+          end
+
+          puts __FILE__ + (__LINE__).to_s + " Process page request #{obj} => #{locator}" if Scoutui::Utils::TestUtils.instance.isDebug?
+
         end
 
+        puts __FILE__ + (__LINE__).to_s + " Locator => #{locator}"
+        puts __FILE__ + (__LINE__).to_s + " Visible_When => #{visible_when}"
 
-        Selenium::WebDriver::Wait.new(timeout: _timeout).until { drv.find_element(:xpath => xpath).displayed? }
-        rc=drv.find_element(:xpath => xpath)
+        Selenium::WebDriver::Wait.new(timeout: _timeout).until { drv.find_element(:xpath => locator).displayed? }
+        rc=drv.find_element(:xpath => locator)
       rescue => ex
-        ;
+        puts "Error during processing: #{$!}"
+        puts "Backtrace:\n\t#{ex.backtrace.join("\n\t")}"
       end
 
-      puts __FILE__ + (__LINE__).to_s + " getObject(#{xpath}) => #{rc.to_s}" if Scoutui::Utils::TestUtils.instance.isDebug?
+      puts __FILE__ + (__LINE__).to_s + " getObject(#{locator}) => #{rc.to_s}" if Scoutui::Utils::TestUtils.instance.isDebug?
+
+#      if rc.kind_of?(Selenium::WebDriver::Element)
+#
+#        if visible_when
+#          puts __FILE__ + (__LINE__).to_s + " Displayed => #{rc.displayed?}"
+#          Testmgr::TestReport.instance.getReq('UI').tc('visible_when').add(rc.displayed?, "Verify #{locator} is visible")
+#        end
+#
+#      end
 
       rc
     end
