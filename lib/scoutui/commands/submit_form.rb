77d7237a51ae31bc283b70a24b3e9763bdeed564
h20dragon <h20dragon@outlook.com>
2016-01-07 23:14:39 -0500

    Update version to 0.1.4.pre

--- lib/scoutui/commands/submit_form.rb
+++ lib/scoutui/commands/submit_form.rb
@@ -4,12 +4,20 @@ module Scoutui::Commands
 
   class SubmitForm < Command
 
+    attr_accessor :form_name
+
+    def initialize(_cmd, _drv=nil)
+      super(_cmd, _drv)
+      @form_name = @cmd.match(/submitform\((.*)\s*\)/)[1].to_s
+    end
+
     def execute(drv=nil)
+
+      puts __FILE__ + (__LINE__).to_s + " Process SubmitForm #{@form_name}"  if Scoutui::Utils::TestUtils.instance.isDebug?
       @drv=drv if !drv.nil?
 
-      _form = _action.match(/submitform\((.*)\s*\)/)[1].to_s
-      _f = Scoutui::Utils::TestUtils.instance.getForm(_form)
-      _f.submitForm(@drv)
+      obj = Scoutui::Utils::TestUtils.instance.getForm(@form_name)
+      obj.submitForm(@drv) if !obj.nil?
     end
 
 
