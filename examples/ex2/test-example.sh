157fdab78fb9393c8e6e2b77cb7f5147e0d8c37b
h20dragon <h20dragon@outlook.com>
2016-01-08 11:14:48 -0500

    Update version to 0.1.4.pre

--- examples/ex2/test-example.sh
+++ examples/ex2/test-example.sh
@@ -9,7 +9,7 @@
 ##
 SCOUTUI_BIN=../../bin/scoutui_driver.rb
 # Specify browser under test  (chrome, firefox, ie, safari)
-BUT=chrome
+BUT=firefox
 
 # Specify the title and appName needed by Applitools
 ## NOTE:  If the test configuration file specifies the title and app, it is superseded by the
@@ -29,10 +29,11 @@ TEST_CFG="./test.config.json"
 MATCH_TYPE="layout"
 
 EYES=--eyes
+EYES=
 
 
 # The following command line parameters will override provided title and appName (if provided in test config file)
-$SCOUTUI_BIN  --config $TEST_CFG  --browser $BUT $EYES --app $APP --title $TITLE  --match $MATCH_TYPE --pagemodel ./page_model.json
+$SCOUTUI_BIN  --config $TEST_CFG  --browser $BUT $EYES --app $APP --title $TITLE  --match $MATCH_TYPE --pagemodel ./page_model.json  --debug
 
 # The following
 # $SCOUTUI_BIN --config $TEST_CFG  --eyes  --match $MATCH_TYPE  --browser $BUT
