625c919d48264b1405d72722ea679c3c6c4aab45
h20dragon <h20dragon@outlook.com>
2016-01-07 23:12:57 -0500

    Update version to 0.1.4.pre

---
+++ lib/scoutui/commands/command.rb
@@ -0,0 +1,39 @@
+
+module Scoutui::Commands
+
+  class Command
+    attr_accessor :description
+    attr_accessor :drv
+    attr_accessor :cmd
+    attr_accessor :rc
+    attr_accessor :locator
+
+    def initialize(_cmd, _drv=nil)
+      puts __FILE__ + (__LINE__).to_s + "  Command: #{_cmd.to_s}"
+      @cmd=_cmd
+      @rc=nil
+      @drv=_drv
+      @locator=nil
+    end
+
+
+    def setLocator(_locator)
+      @locator=_locator
+    end
+
+    def getLocator()
+      @locator
+    end
+
+    def result
+      @rc
+    end
+
+    def execute(_drv=nil, _dut=nil)
+      raise NotImplementedError
+    end
+
+  end
+
+
+end
