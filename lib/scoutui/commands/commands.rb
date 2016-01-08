4fddca6e44b757403243e88315447792ece2c0ad
h20dragon <h20dragon@outlook.com>
2016-01-07 23:13:18 -0500

    Update version to 0.1.4.pre

--- lib/scoutui/commands/commands.rb
+++ lib/scoutui/commands/commands.rb
@@ -1,28 +1,97 @@
+require_relative('./command')
 
 module Scoutui::Commands
 
-  class Command
-    attr_accessor :description
-    attr_accessor :drv
-    attr_accessor :cmd
-    attr_accessor :rc
+  STEP_KEY='page'
 
-    def initialize(_cmd, _drv=nil)
-      puts __FILE__ + (__LINE__).to_s + "  Command: #{_cmd.to_s}"
-      @cmd=_cmd
-      @rc=nil
-      @drv=_drv
-    end
 
-    def result
-      @rc
-    end
+  def self.processCommands(commandList, my_driver)
 
-    def execute(_drv=nil, _dut=nil)
-      raise NotImplementedError
+    commandList.each do |cmd|
+      begin
+        rc=processCommand(cmd[:command], cmd[:e], my_driver)
+      rescue => ex
+        puts "Error during processing: #{$!}"
+        puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+      end
+
     end
 
   end
+
+  def self.processCommand(_action, e, my_driver)
+    puts __FILE__ + (__LINE__).to_s + " ===  Process ACTION : #{_action}  ==="  if Scoutui::Utils::TestUtils.instance.isDebug?
+
+    rc=true
+
+    begin
+
+      _c=nil
+
+      if Scoutui::Commands::Utils.instance.isPause?(_action)
+        _c = Scoutui::Commands::Pause.new(nil)
+        _c.execute();
+
+      elsif Scoutui::Commands::Utils.instance.isClick?(_action)
+        _c = Scoutui::Commands::ClickObject.new(_action)
+        _c.execute(my_driver)
+
+      elsif  Scoutui::Commands::Utils.instance.isMouseOver?(_action)
+
+        _c = Scoutui::Commands::MouseOver.new(_action)
+        _c.execute(my_driver)
+
+      elsif Scoutui::Commands::Utils.instance.isSelect?(_action)
+        _c = Scoutui::Commands::SelectObject.new(_action)
+        _c.execute(my_driver)
+
+      elsif Scoutui::Commands::Utils.instance.isFillForm?(_action)
+
+        #   _c = Scoutui::Commands::FillForm.new(_action)
+
+        _form = _action.match(/fillform\((.*)\s*\)/)[1].to_s
+        #  _dut = _action.match(/fillform\(.*,\s*(.*)\)/)[1].to_s
+
+        dut = e[STEP_KEY]['dut']
+
+        puts __FILE__ + (__LINE__).to_s + " DUT => #{dut}"
+        _f = Scoutui::Utils::TestUtils.instance.getForm(_form)
+        _f.dump()
+        _f.verifyForm(my_driver)
+        _f.fillForm(my_driver, dut)
+
+      elsif Scoutui::Commands::Utils.instance.isSubmitForm?(_action)
+        _c = Scoutui::Commands::SubmitForm.new(_action)
+        _c.execute(my_driver)
+
+
+      elsif Scoutui::Commands::Utils.instance.isNavigate?(_action)
+        _c = Scoutui::Commands::UpdateUrl.new(_action)
+        _c.execute(my_driver)
+
+      elsif Scoutui::Commands::Utils.instance.isVerifyElt?(_action)
+
+        _c = Scoutui::Commands::VerifyElement.new(_action)
+        _c.execute(my_driver)
+
+      elsif Scoutui::Commands::Utils.instance.isType?(_action)
+        _c = Scoutui::Commands::Type.new(_action)
+        _c.execute(my_driver)
+      else
+        rc=false
+      end
+
+    rescue => e
+      puts "Error during processing: #{$!}"
+      puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+      rc=false
+    end
+
+    rc
+  end
+
+
+
 
 
 end
