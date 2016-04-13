62965342158cd08595f5c83f2e79bb8b3808f174
Matt Jonas <matt.jonas@gmail.com>
2016-04-12 17:16:51 -0700

    Merge ex3 into ex2

--- examples/ex2/tests/test-basic-appmodel.sh
+++ examples/ex2/tests/test-basic-appmodel.sh
@@ -7,8 +7,8 @@ export SCOUTUI_BIN=../../../bin/scoutui_driver.rb
 
 
 # Specify the test configuration file
-TEST_CFG="../test-configs/test.config.basic.json"
+TEST_CFG="../test-configs/test.config.json"
 APP_MODEL="../appmodel/page_model.json"
 
 
-./run-test.sh -b chrome -d ../commands/commands.basic.appmodel.yml -P $APP_MODEL  -t $TEST_CFG -v
+./run-test.sh -b chrome -d ../commands/ex2.yml -P $APP_MODEL  -t $TEST_CFG -v -e -i
